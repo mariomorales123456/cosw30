dba90ee09d80e40391196768e91d2402034a780f
EC2 Default User <ec2-user@ip-172-31-90-244.ec2.internal>
2019-08-29 18:40:04 +0000

    added rest of requirements

--- cosw30-fall19/labwork/lab1.php
+++ cosw30-fall19/labwork/lab1.php
@@ -1,14 +1,49 @@
+/*
+Mario Morales
+Lab 1 PHP 
+8/28/19
+*/
+
 <!DOCTYPE html>
 <html>
     <head>
         <title>My First PHP Page</title>
-        <style></style>
+        <style>
+            body {
+                background-color: blue;
+                color: white;
+            }
+            h1 {
+                font-style: italic;
+            }
+            
+        </style>
     </head>
     <body>
         <h1>Mario Morales</h1>
-        <p>"Random Gibberish"</p>
+        <p>I've been in the computer science and web dev field for a year now. I have previously
+            gone to school for a bachelor's in animal health sciences, basically veterinary
+            nurse. I did that for 3 years but chose to change fields. This is one of my last classes to 
+            graduate with an AS degree.
+        </p>
+        <h2>Few of my hobbies:</h2>
+        <ul>
+            <li>Gaming</li>
+            <li>Spending time with family</li>
+            <li>Spending time with friends</li>
+        </ul>
+        <p>
+        <a href="https://github.com/mariomorales123456/cosw30">My GitHub!</a>
+        </p>
         <?php
-            echo 'This is my PHP beautiful paragraph';
+            echo '<p>I spend a lot of time on <a href="www.reddit.com">Reddit</a> main page
+            because it is my main source of major news. Im also a TCG nerd so I look at
+            <a href="www.mtgstocks.com">MTGstocks</a> a website that lists changes in card 
+            prices. </p>
+            <p>This assignment was really difficult just because there was so many different
+            steps in setting up three different accounts. However the actual coding part
+            is not too bad.</p>
+            ';
         ?>
     </body>
 </html>
