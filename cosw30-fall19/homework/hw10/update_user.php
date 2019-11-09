2bb9741d24bca7f06bf04e2b93072536dac24d14
EC2 Default User <ec2-user@ip-172-31-90-244.ec2.internal>
2019-11-09 18:45:53 +0000

    updated hw10

--- cosw30-fall19/homework/hw10/update_user.php
+++ cosw30-fall19/homework/hw10/update_user.php
@@ -56,31 +56,41 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
         <title>
             Update User Form
         </title>
+        <link rel="stylesheet" type="text/css" href="styles.css">
     </head>
     <body>
+        <div class="fg">
+         <h1>Update User</h1>
+        <div class="auto_margin">
+        <form action="update_user.php" method="POST">
+        <input type="hidden" name="user_id" value="<?php echo $user_id ?>">
+        <div class="cssTable">
+        <div class="cssRow">
+        <label for="first_name">First Name</label>
+        <input type="text" id="first_name" name="first_name" value="<?php echo $first_name ?>"><br>
+        </div>
+        <div class="cssRow">
+        <label for="last_name">Last Name</label>
+        <input type="text" id="last_name" name="last_name" value="<?php echo $last_name ?>"><br>
+        </div>
+        <div class="cssRow">
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" value="<?php echo $email ?>"><br>
+        </div>
+        <div class="cssRow">
+        <label for="password">Password</label>
+        <input type="text" id="password" name="password" value="<?php echo $password ?>"><br>
+        </div>
+        <div class="cssRow">
+        <label for="confirm_password">Confirm Password</label>
+        <input type="text" id="confirm_password" name="confirm_password"><br>
+        </div>
+        </div>
         <div>
-        <h1>Update User</h1>
-        <form class="auto_margin" action="update_user.php" method="POST">
-            
-            <input type="hidden" name="user_id" value="<?php echo $user_id ?>">
-            
-            <label for="first_name">First Name</label>
-            <input type="text" id="first_name" name="first_name" value="<?php echo $first_name ?>"><br>
-
-            <label for="last_name">Last Name</label>
-            <input type="text" id="last_name" name="last_name" value="<?php echo $last_name ?>"><br>
-
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" value="<?php echo $email ?>"><br>
-
-            <label for="password">Password</label>
-            <input type="text" id="password" name="password" value="<?php echo $password ?>"><br>
-            
-            <label for="confirm_password">Confirm Password</label>
-            <input type="text" id="confirm_password" name="confirm_password"><br>
-
-            <input type="submit" value="Update">
+        <input class="submitBtn" type="submit" value="Update">
+        </div>
         </form>
+        </div>
         </div>
     </body>
 </html>
